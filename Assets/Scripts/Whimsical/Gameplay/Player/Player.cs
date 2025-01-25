c362103936628b86c269848b7474866a609def45
Antonio Escorcia <packageinstaller2@gmail.com>
2025-01-25 16:49:35 -0600

    feat: same keyboard for both players

--- Assets/Scripts/Whimsical/Gameplay/Player/Player.cs
+++ Assets/Scripts/Whimsical/Gameplay/Player/Player.cs
@@ -8,38 +8,42 @@ namespace Whimsical.Gameplay.Player
     using UnityEngine.InputSystem;
 
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(PlayerInput))]
     public class Player : MonoBehaviour
     {
         [SerializeField]
         private CharacterStats _stats;
         private HealthPoints _healthPoints;
 
+        private Rigidbody2D _rb;
+        
+        private PlayerInput _input;
         private const string JumpAction = "Jump";
         private InputAction _jumpAction;
 
         private const string MoveAction = "Move";
         private InputAction _moveAction;
 
-        private Rigidbody2D _rb;
-
         private void Start()
         {
             // Instantiating stuff
             _healthPoints = new HealthPoints(_stats.BaseMaxHealth);
+            DebugExtensions.Log($"My health is {_healthPoints.CurrentHealth}");
             
             // Getting stuff
             _rb = this.GetComponent<Rigidbody2D>();
+            _input = this.GetComponent<PlayerInput>();
             
             // Initializing input
-            _jumpAction = InputSystem.actions.FindAction(JumpAction);
-            _moveAction = InputSystem.actions.FindAction(MoveAction);
+            _jumpAction = _input.actions.FindAction(JumpAction);
+            _moveAction = _input.actions.FindAction(MoveAction);
             
-            DebugExtensions.Log($"My health is {_healthPoints.CurrentHealth}");
+            _input.SwitchCurrentControlScheme(_input.defaultControlScheme, Keyboard.current);
         }
 
         private void FixedUpdate()
         {
-            var movement = _moveAction.ReadValue<Vector2>().normalized.x;
+            var movement = _moveAction.ReadValue<float>();
             _rb.linearVelocityX = movement * _stats.MovementSpeed;
         }
     }
