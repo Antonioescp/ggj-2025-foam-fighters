8e011f1ae4c703e16b2ce2dfe041ca856874dad5
Antonio Escorcia <packageinstaller2@gmail.com>
2025-01-25 09:53:05 -0600

    Base project

---
+++ Assets/Scripts/Gameplay/HelloWorld.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("We are so back");
+        }
+    }
+}
